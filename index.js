c573b2431b33d0fe50db2844c9b161cc16c42853
dwanerich <dwanerich@gmail.com>
2020-09-22 01:19:04 -0400

    Done.

--- index.js
+++ index.js
@@ -34,10 +34,11 @@ class Bird {
     speak() {
 
         if (`${this.sex} = male`) {
-            return `It's me! ${this.name}, the parrot!`
-        } else {
             return `${this.name} says squawk!`
+        } else {
+            return `It's me! ${this.name} the parrot!`
         }
     }
 }
 
+bird2.speak()
